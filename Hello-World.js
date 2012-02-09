5618ffb7b7f5dbbdcf2db41bb9f3a28a6f71c37b
Garen Keshishian <gkeshishian@cinemoz.com>
2012-02-09 11:36:26 +0200

    first commit

---
+++ Hello-World.js
@@ -0,0 +1,14 @@
+Ext.Loader.setConfig({
+    enabled: true
+});
+
+Ext.application({
+    name: 'v2',
+
+    launch: function() {
+        Ext.QuickTips.init();
+
+Ext.Msg.alert('greeting msg', 'Hello-World');
+
+    }
+});
